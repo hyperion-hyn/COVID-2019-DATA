67f6be920373958d4f8dee064439f18b3d820f78
naruto.j <15115790290@hot.com>
2020-03-04 14:08:40 +0800

    Update simple.js

--- web/src/pages/main/demo_charts/simple.js
+++ web/src/pages/main/demo_charts/simple.js
@@ -2,7 +2,9 @@ import React, { PureComponent } from 'react';
 import ReactEcharts from 'echarts-for-react';
 
 export default class Simple extends PureComponent {
-  getOption = () => {
+
+  
+  getOption_demo = () => {
     return {
       title: {
         text: '堆叠区域图'
@@ -61,23 +63,97 @@ export default class Simple extends PureComponent {
       ]
     };
   };
+
+  basicOption = () => {
+    return {
+      xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+          type: 'value'
+      },
+      series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line'
+      }]
+    };
+  };
+
+  barOption = () => {
+    return {
+      title: {
+          text: '韩国形势',
+      },
+      tooltip: {
+          trigger: 'axis'
+      },
+      legend: {
+          left: 'center',
+          bottom: '2%',
+          data: ['新增确诊', '新增康复']
+      },
+      calculable: true,
+      xAxis: [
+          {
+              type: 'category',
+              data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+          }
+      ],
+      yAxis: [
+          {
+              type: 'value'
+          }
+      ],
+      series: [
+          {
+              name: '新增确诊',
+              type: 'bar',
+              data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+              markPoint: {
+                  data: [
+                      {type: 'max', name: '最大值'},
+                      {type: 'min', name: '最小值'}
+                  ]
+              },
+              markLine: {
+                  data: [
+                      {type: 'average', name: '平均值'}
+                  ]
+              }
+          },
+          {
+              name: '新增康复',
+              type: 'bar',
+              data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+              markPoint: {
+                  data: [
+                      {name: '年最高', value: 182.2, xAxis: 7, yAxis: 183},
+                      {name: '年最低', value: 2.3, xAxis: 11, yAxis: 3}
+                  ]
+              },
+              markLine: {
+                  data: [
+                      {type: 'average', name: '平均值'}
+                  ]
+              }
+          }
+      ]
+    };
+  }
+
   render() {
-    let code = "<ReactEcharts \n" +
-          "  option={this.getOption()} \n" +
-          "  style={{height: '350px', width: '100%'}}  \n" +
-          "  className='react_for_echarts' />";
     return (
       <div className='examples'>
         <div className='parent'>
-          <label> render a Simple echart With <strong>option and height</strong>: </label>
           <ReactEcharts
-            option={this.getOption()}
+            option={this.barOption()}
             style={{height: '350px', width: '100%'}}
             className='react_for_echarts' />
-          <label> code below: </label>
-          <pre>
-            <code>{code}</code>
-          </pre>
+          <ReactEcharts
+            option={this.basicOption()}
+            style={{height: '350px', width: '100%'}}
+            className='react_for_echarts' />
         </div>
       </div>
     );
